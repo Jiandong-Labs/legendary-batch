c2aee1727774db03c70efa5bc489afd62c2e5c85
Jiandong Ma <jiandong.ma.cn@gmail.com>
2026-06-17 11:48:06 +0800

    initial commit
    Signed-off-by: Jiandong Ma <jiandong.ma.cn@gmail.com>

---
+++ src/main/java/com/jiandong/legendarybatch/LegendaryBatchApplication.java
@@ -0,0 +1,13 @@
+package com.jiandong.legendarybatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendaryBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegendaryBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiandong/legendarybatch/LegendaryBatchApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jiandong.legendarybatch;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegendaryBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
